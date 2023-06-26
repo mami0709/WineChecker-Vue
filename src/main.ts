c40ec2f20455041bc9dea3e5ccf4ecdd1538c799
mami0709 <mmmxxxzelda@yahoo.co.jp>
2023-06-26 23:39:54 +0900

    ChakraUI追加

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,14 @@
 import './assets/main.css'
 
-import { createApp } from 'vue'
+import { createApp, h } from 'vue'
 import App from './pages/App.vue'
-import Chakra from '@chakra-ui/vue-next'
-import { h } from 'vue'
+import ChakraUIVuePlugin from '@chakra-ui/vue-next'
 import { store } from './store/index'
 
 const app = createApp({
   render: () => h(App)
 })
 
-app.use(Chakra)
+app.use(ChakraUIVuePlugin)
 app.use(store)
 app.mount('#app')
